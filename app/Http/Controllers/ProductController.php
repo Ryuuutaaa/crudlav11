65b946ab53616aa0a52cdfa23476ba518bfdc1ab
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-04-03 02:46:41 +0800

    get started crud in page create

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //show product
+    public function index()
+    {
+    }
+
+    // create product
+    public function create()
+    {
+        return view("products.create");
+    }
+
+    // store product
+    public function store()
+    {
+    }
+
+    // edit product
+    public function edit()
+    {
+    }
+
+    // update product
+    public function update()
+    {
+    }
+
+    // delete product
+    public function destroy()
+    {
+    }
+}
